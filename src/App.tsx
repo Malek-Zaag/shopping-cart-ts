3ae7c59a66d6d829206335da88cbaf37591c3944
unknown <zaag.malek1@gmail.com>
2022-07-10 17:40:07 +0100

    project changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Container } from "react-bootstrap";
-import { Navbar } from "../components/Navbar";
+import { Navbar } from "./components/Navbar";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Store } from "../pages/Store";
-import { About } from "../pages/About";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
 
 function App() {
   const [count, setCount] = useState(0);
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,5 @@
 import { Button, Card } from "react-bootstrap";
+import { formatCurrency } from "../utils/formatCurrency";
 
 type Props = {
   id: number;
@@ -9,16 +10,19 @@ type Props = {
 
 export function StoreItem({ id, name, price, imgUrl }: Props) {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card className="h-100" style={{ width: "18rem" }}>
       <Card.Img
         variant="top"
         src={imgUrl}
         height="200px"
         style={{ objectFit: "cover" }}
       />
-      <Card.Body>
-        <Card.Title>{name}</Card.Title>
-        <Card.Text>{price}</Card.Text>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted"></span>
+        </Card.Title>
+        <Card.Text>{formatCurrency(price)}</Card.Text>
         <Button variant="primary"></Button>
       </Card.Body>
     </Card>
